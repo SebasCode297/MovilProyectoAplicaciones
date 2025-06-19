9259ec727b1199009e19e3e4e0d29392ca61eb58
SebasCode297 <carlospilamunga12@gmail.com>
2025-06-19 10:56:26 -0500

    abtes de examen

--- src/pantallas/consumoAPI/consumoAPI.jsx
+++ src/pantallas/consumoAPI/consumoAPI.jsx
@@ -1,16 +1,45 @@
 import { Card } from "../../componentes/card/card";
-import {Container} from "../../componentes/container/container";
+import { Container } from "../../componentes/container/container";
+import { useState, useEffect } from "react";
+import { ActivityIndicator, FlatList } from "react-native";
 
 export const ConsumoAPI = () => {
-    return <Container>
-        <Card url ="https://www.eldiario.ec/wp-content/uploads/elementor/thumbs/Liga-Deportiva-Universitaria-de-Quito-LDU-aseguro-su-clasificacion-a-los-octavos-de-final-de-la-Copa-Libertadores-2025-r6i48eskesztax2nciq54umb71zxwonvvgxzgjs4eq.jpg
-        "
-        nombre="Rick Sanchez"
-        especie="Human"        
-        
-        />
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
 
+    const getPersonajes = async () => {
+        try {
+            const response = await fetch("https://rickandmortyapi.com/api/character");
+            const json = await response.json();
+            setData(json.results);
+        } catch (error) {
+            console.log("Error: " + error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getPersonajes();
+    }, []);
+
+    return (
+        <Container>
+            {loading ? (
+                <ActivityIndicator/>
+            ) : (
+                <FlatList
+                    data={data}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({ item }) => (
+                        <Card
+                            url={item.image}
+                            nombre={item.name}
+                            especie={item.species}
+                        />
+                    )}
+                />
+            )}
         </Container>
-        
+    );
 };
-
